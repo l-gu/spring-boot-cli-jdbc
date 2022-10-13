c35a848b5261c1f8d00c54af6e1afd7824f92282
l-gu <laurent@telosys.org>
2022-10-13 14:31:51 +0200

    Tests : IT example

---
+++ src/test/java/org/demo/db/EmployeeRepositoryIT.java
@@ -0,0 +1,14 @@
+package org.demo.db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeRepositoryIT {
+
+	@Test
+	void test() {
+		// TODO
+	}
+
+}
